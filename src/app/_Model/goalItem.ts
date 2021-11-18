1e854d33e29f8ffbf68a55138575e5e2be674653
dominik <noneofyourbusiness@fu.fu>
2021-11-18 07:28:15 +0100

    changes to goaldetails

--- src/app/_Model/goalItem.ts
+++ src/app/_Model/goalItem.ts
@@ -5,6 +5,11 @@ export class GoalItem {
   categoryA: 'funded' | 'semi-funded' | 'near' | 'far' | null  = null;
   categoryB: 'perennial' | 'one-time-long' | 'one-time-short' | null = null;
   juicyness:  0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | null = null;
+  primaryResource: 'time' | 'money' | 'time AND money' = 'time'
+  actionCategory : 'purchase' | 'practise' | 'project' | 'meta-action' | 'other' | null = null
+  actionGoalField : 'action' | 'goal' | 'field' = 'action'
+  resoucesNeeded: number = 0
+  resourcesAssigned: number = 0
 
 
   constructor() {
