12156fe4f61e453e640bf2bb24e4cef7283d314e
Dominik Heilscher <dominik.heilscher@gmail.com>
2021-12-30 13:24:57 +0100

    0.16.3 : goaldetails : removed useless 'action category' (rudundant with secondary Category)

--- src/app/_Model/goalItem.ts
+++ src/app/_Model/goalItem.ts
@@ -8,7 +8,7 @@ export class GoalItem {
   categoryB: 'perennial' | 'one-time-long' | 'one-time-short' | null = null;
   juicyness:  0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | null = null;
   primaryResource: 'time' | 'money' | 'time AND money' = 'time'
-  actionCategory : 'purchase' | 'practise' | 'project' | 'meta-action' | 'other' | null = null
+  actionCategory : 'purchase' | 'practise' | 'project' | 'meta-action' | 'other' | null = null   //  Deprecated : not yet deleted for reasons of save backwards compatibility
   actionGoalField : string = 'goal'
   actionGoalFieldSecondary : string = 'undefined'
   resoucesNeeded: number = 0
